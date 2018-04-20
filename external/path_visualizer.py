c03b27b06b26703536e0d723be5742e2b71629a0
Tobias Lundby <tobiaslundby@gmail.com>
2018-04-20 11:39:56 +0200

    The class for sending paths can now delete paths and the visualizer funciton has been finalized so it tries to determine the input by itself so 3 different types can be defined

--- external/path_visualizer.py
+++ external/path_visualizer.py
@@ -14,8 +14,12 @@ License: BSD 3-Clause
 
 #print 'Importing libraries'
 import requests
+import sys
 from termcolor import colored
 #print 'Import done\n'
+
+""" User defines """
+DEFAULT_AID = 900
 
 class path_visualizer():
     def __init__(self, debug = False):
@@ -23,17 +27,35 @@ class path_visualizer():
     def visualize_path(self, path, aid=False):
         # If aid is not specified the data must be given as DICT with aid embedded. If aid is specified the path needs to be a simple 4 element array og lat, lon, alt, time
         # Check if there is any data
-        if  isinstance(path, list) and len(path) > 0:
+        if isinstance(path, list) and len(path) > 0:
             path_JSON = []
             # some code for generating the right format
             if aid == False:
                 # code
-                for element in path:
-                    path_JSON.append({'aid': element['aid'],'lat': element['lat'],'lon': element['lng'],'alt': element['alt'],'time': element['time']})
-                print path_JSON
+                try:
+                    tmp_var = path[0]['lat']
+                except TypeError:
+                    print colored("Input not as expected; it seems as the path provided is a 4 value array containing lat, lon, alt, time but missing aid", 'red')
+                    sys.exit(1)
+                else:
+                    try:
+                        tmp_var = path[0]['aid']
+                    except KeyError:
+                        if self.debug:
+                            print colored('No AID specified so it is defaulted to %d' % DEFAULT_AID, 'yellow')
+                        for element in path:
+                            path_JSON.append({'aid': DEFAULT_AID,'lat': element['lat'],'lon': element['lng'],'alt': element['alt'],'time': element['time']})
+                    else:
+                        for element in path:
+                            path_JSON.append({'aid': element['aid'],'lat': element['lat'],'lon': element['lng'],'alt': element['alt'],'time': element['time']})
             else:
-                # code
-                arr = []
+                if len(path[0]) == 4:
+                    for element in path:
+                        path_JSON.append({'aid': aid,'lat': element[0],'lon': element[1],'alt': element[2],'time': element[3]})
+
+            if self.debug:
+                print path_JSON
+
             # send the path to http://uas.heltner.net/routes
             try:
                 r = requests.post("http://uas.heltner.net/routes", json=path_JSON, timeout=2)
@@ -56,6 +78,29 @@ class path_visualizer():
                         print colored('Path uploaded, route ID %s: %s, %s' % (r.text.strip(), 'http://uas.heltner.net/routes/'+r.text.strip()+'/2d', 'http://uas.heltner.net/routes/'+r.text.strip()+'/3d'), 'green')
                 else:
                     print colored('Path NOT uploaded', 'red')
+    def delete_route(self, routeid):
+        if isinstance(routeid, (int, long)):
+            try:
+                r = requests.delete("http://uas.heltner.net/routes", json={'routeid': routeid}, timeout=2)
+            except requests.exceptions.Timeout:
+                # Maybe set up for a retry, or continue in a retry loop
+                print colored('Request has timed out', 'red')
+            except requests.exceptions.TooManyRedirects:
+                # Tell the user their URL was bad and try a different one
+                print colored('Request has too many redirects', 'red')
+            except requests.exceptions.RequestException as e:
+                # catastrophic error. bail.
+                print colored('Request error', 'red')
+                print colored(e, 'yellow')
+                # sys.exit(1)
+            else:
+                #print(r.status_code, r.reason)
+                #print(r.text)
+                if r.status_code == 200 and r.reason == 'OK':
+                    if self.debug:
+                        print colored('Path/route deleted, route ID %d' % routeid, 'green')
+                else:
+                    print colored('Path/route NOT deleted', 'red')
 
 
 if __name__ == '__main__':
@@ -89,10 +134,16 @@ if __name__ == '__main__':
             'time': 1524162482
         }
     ]
+    TEST_DATA = [
+        [55.399, 10.385, 50, 1524162422],
+        [55.400, 10.386, 100, 1524162482]
+    ]
 
     path_visualizer_module = path_visualizer(True)
-    path_visualizer_module.visualize_path(LOG_TEST_DATA_wAID)
-
+    #path_visualizer_module.visualize_path(LOG_TEST_DATA_wAID)
+    #path_visualizer_module.visualize_path(LOG_TEST_DATA_woAID)
+    #path_visualizer_module.visualize_path(TEST_DATA)
+    path_visualizer_module.delete_route(100)
 
     arr = []
     arr.append({'aid': 900,'lat': 55.400,'lon': 10.385,'alt': 50})
