8f33ce680d562598651c337c542aac102a2acb02
Tobias Lundby <tobiaslundby@gmail.com>
2018-05-14 13:38:14 +0200

    Second GUI version

--- libs/gui.py
+++ libs/gui.py
@@ -6,10 +6,10 @@ Description: GUI class for the path planner
     License: BSD 3-Clause
 """
 
-import threading
 from Tkinter import Tk, Label, Button
-
-root = Tk() # create master/root object
+import threading
+from Queue import Queue, Empty
+import time
 
 class StoppableThread(threading.Thread):
     def __init__(self):
@@ -24,39 +24,76 @@ class StoppableThread(threading.Thread):
             self.join()
 
 class path_planner_gui(StoppableThread):
-    def __init__(self, master):
+
+    def __init__(self, auto_start = False):
         StoppableThread.__init__(self)
-        self.master = master
-        master.title("UAV Path Planning")
+        self.q = Queue()
+        if auto_start:
+            self.start()
 
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
+    def callback(self):
+        self.root.quit()
+        print "Tkinter GUI has stopped but thread will first be joined upon closing of the program"
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+    def on_main_thread(self, func):
+        self.q.put(func)
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+    def check_queue(self):
+        while True:
+            try:
+                task = self.q.get(block=False)
+            except Empty:
+                break
+            else:
+                self.root.after_idle(task)
+        self.root.after(100, self.check_queue)
 
-    def run(self):
-        self.master.mainloop()
+    def set_cnt_label(self, txt):
+        com_txt = 'count is %i' % txt
+        print 'Tkinter: count is %i' % txt
+        self.label_cnt.configure(text=com_txt)
 
     def greet(self):
-        self.label.configure(text="It works")
-        print("Greetings!")
+        self.label.configure(text="Greetings!")
+        self.greet_button.configure(text="Ungreet!")
+        self.greet_button.configure(command=self.ungreet)
+    def ungreet(self):
+        self.label.configure(text="No greetings for you!")
+        self.greet_button.configure(text="Greet!")
+        self.greet_button.configure(command=self.greet)
+
+    def run(self):
+        self.root = Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+
+        self.label_cnt = Label(self.root, text="Hello World")
+        self.label_cnt.pack()
+
+        self.label = Label(self.root, text="")
+        self.label.pack()
+
+        self.greet_button = Button(self.root, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.root.after(100, self.check_queue)
+
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    pp_gui = path_planner_gui(root)
+    pp_gui = path_planner_gui()
     pp_gui.start()
 
-    #root.update()
-    print 1+1
+    print('Now we can continue running code while mainloop runs!')
 
+    var = 1
     do_exit = False
     while do_exit == False:
         try:
             time.sleep(0.1)
-            print 1+1
+            var += 1
+            print 'Main: count is %i' % var
+            pp_gui.on_main_thread( lambda: pp_gui.set_cnt_label(var) )
+
         except KeyboardInterrupt:
             # Ctrl+C was hit - exit program
             do_exit = True
